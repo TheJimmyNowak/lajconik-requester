fcb0c7ed213c2d8b0be7342f9abe8a9a97fe3c6e
miki164 <miki3867@gmail.com>
2021-03-17 14:58:07 +0100

    Repair get_random_str

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def post_request(requests_count: int, url: str) -> None:
 
 
 def get_random_str(length: int):
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+    return ''.join(random.choice(string.ascii_letters + string.digits + string.whitespace) for _ in range(random.randint(0, length)))
 
 
 def get_random_json():
